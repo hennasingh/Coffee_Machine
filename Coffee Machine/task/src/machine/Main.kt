c0cf575c0c3633b2f12c8cdbfc922878655ef7d8
Henna Singh <hennasingh04@gmail.com>
2020-09-12 00:22:14 +0100

    Implemented Stage 4 of the Coffee Machine

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -2,45 +2,103 @@ package machine
 
 import java.util.*
 
+var waterAvailable = 400
+var milkAvailable = 540
+var beansAvailable = 120
+var cupsAvailable = 9
+var moneyAvailable = 550
+val scanner = Scanner(System.`in`)
+
 fun main() {
-    println("Starting to make a coffee")
-    println("Grinding coffee beans")
-    println("Boiling water")
-    println("Mixing boiled water with crushed coffee beans")
-    println("Pouring coffee into the cup")
-    println("Pouring some milk into the cup")
-    println("Coffee is ready!")
 
-    println("Write how many cups of coffee you will need: ")
+    availableContents()
 
-    val scanner = Scanner(System.`in`)
+    println("Write action (buy, fill, take): ")
 
-    println("Write how many ml of water the coffee machine has: ")
-    val waterAvailable = scanner.nextInt()
+    when (scanner.next()) {
+        "buy" -> actionBuy()
+        "fill" -> actionFill()
+        "take" -> actionTake()
+        else -> println("Unknown Operation")
+    }
+}
 
-    println("Write how many ml of milk the coffee machine has: ")
-    val milkAvailable = scanner.nextInt()
+fun actionTake() {
 
-    println("Write how many grams of coffee beans the coffee machine has: ")
-    val coffeeBeansAvailable = scanner.nextInt()
+    println("I gave you $$moneyAvailable")
+    moneyAvailable -= moneyAvailable
+    availableContents()
+}
 
-    val coffeeCupsPossible = minOf(waterAvailable/200 , milkAvailable/50, coffeeBeansAvailable/15)
+fun actionFill() {
+    println("Write how many ml of water do you want to add: ")
+    waterAvailable += scanner.nextInt()
 
-    println("Write how many cups of coffee you will need: ")
-    val coffeeCupsNeeded = scanner.nextInt()
+    println("write how many ml of milk do you want to add: ")
+    milkAvailable += scanner.nextInt()
 
-    when {
-        coffeeCupsNeeded == coffeeCupsPossible -> {
-            println("Yes, I can make that amount of coffee")
-        }
-        coffeeCupsNeeded < coffeeCupsPossible -> {
-            println(" Yes, I can make that amount of coffee ( and even ${coffeeCupsPossible - coffeeCupsNeeded} more than that")
-        }
-        else -> {
-            println("No, I can make only $coffeeCupsPossible cups of coffee")
-        }
+    println("Write how many grams of coffee beans do you want to add: ")
+    beansAvailable += scanner.nextInt()
+
+    println("Write how many disposable cups of coffee do you want to add: ")
+    cupsAvailable += scanner.nextInt()
+
+    availableContents()
+}
+
+private fun availableContents() {
+    println("The coffee machine has: ")
+    println("$waterAvailable ml of water")
+    println("$milkAvailable ml of milk")
+    println("$beansAvailable g of coffee beans")
+    println("$cupsAvailable of disposable cups")
+    println("$moneyAvailable of money")
+}
+
+fun actionBuy() {
+
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+
+    when (scanner.nextInt()) {
+        1 -> buyEspresso()
+        2 -> buyLatte()
+        3 -> buyCappuccino()
+    }
+}
+
+fun buyCappuccino() {
+
+    if (waterAvailable > 200 && beansAvailable > 12 && milkAvailable > 100 && cupsAvailable > 1) {
+        waterAvailable -= 200
+        beansAvailable -= 12
+        milkAvailable -= 100
+        moneyAvailable += 6
+        cupsAvailable -= 1
+        availableContents()
     }
 
+}
 
+fun buyLatte() {
 
+    if (waterAvailable > 350 && beansAvailable > 20 && milkAvailable > 75 && cupsAvailable > 1) {
+        waterAvailable -= 350
+        beansAvailable -= 20
+        milkAvailable -= 75
+        moneyAvailable += 7
+        cupsAvailable -= 1
+        availableContents()
+    }
+
+}
+
+fun buyEspresso() {
+
+    if (waterAvailable > 250 && beansAvailable > 16 && cupsAvailable > 1) {
+        waterAvailable -= 250
+        beansAvailable -= 16
+        moneyAvailable += 4
+        cupsAvailable -= 1
+        availableContents()
+    }
 }
